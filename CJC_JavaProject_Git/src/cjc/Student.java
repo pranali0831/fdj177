fb994556ae3da5bf1cf2a07e2a5396c75577153d
pranali0831 <pranalibhanage081@gmail.com>
2024-07-09 18:01:39 +0530

    Update Student.java
    add m1 method

--- CJC_JavaProject_Git/src/cjc/Student.java
+++ CJC_JavaProject_Git/src/cjc/Student.java
@@ -1,7 +1,10 @@
 package cjc;
 
 public class Student {
-	
+	public void m1()
+	{
+		System.out.println("m1---method");
+	}
 	
 public static void main(String[] args) {
 	System.out.println("1st git project");
